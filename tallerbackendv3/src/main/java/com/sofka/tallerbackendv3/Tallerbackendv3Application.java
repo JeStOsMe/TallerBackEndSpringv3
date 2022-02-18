f369a52353f0548edc1b86e418aec17e161554df
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-02-17 21:37:44 -0500

    Primer commit. Inicio del proyecto. Estado: recién descomprimido del Spring Initializr

---
+++ tallerbackendv3/src/main/java/com/sofka/tallerbackendv3/Tallerbackendv3Application.java
@@ -0,0 +1,13 @@
+package com.sofka.tallerbackendv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tallerbackendv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tallerbackendv3Application.class, args);
+	}
+
+}
---
+++ tallerbackendv3/src/test/java/com/sofka/tallerbackendv3/Tallerbackendv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.tallerbackendv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tallerbackendv3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
