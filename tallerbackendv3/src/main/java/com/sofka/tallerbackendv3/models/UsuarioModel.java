92dd647404cf50060d62e935cab78fa249317918
Jeaniel Osorno <jeanielosorno@gmail.com>
2022-02-17 22:39:23 -0500

    Getters y setters agregados para UsuarioModel

--- tallerbackendv3/src/main/java/com/sofka/tallerbackendv3/models/UsuarioModel.java
+++ tallerbackendv3/src/main/java/com/sofka/tallerbackendv3/models/UsuarioModel.java
@@ -28,6 +28,38 @@ public class UsuarioModel {
     private String nombre;
     private String email;
     private Integer prioridad;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
     
     
 }
